cfcfa2c431493da4300ed24f7a2c1253478ff735
Kate Bennert <kate.bennert@gmail.com>
2022-09-20 12:59:48 -0400

    commit tree 636b65b2db2bd57747dac356982d91c696e526db

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,40 @@ import React, { useState } from "react";
 import ShoppingList from "./ShoppingList";
 import Header from "./Header";
 import itemData from "../data/items";
+import { v4 as uuid } from "uuid";
 
 function App() {
-  const [items, setItems] = useState(itemData);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [items, setItems] = useState(itemData);
+  const [name, setName] = useState("");
+  const [category, setCategory] = useState("Produce");
 
   function handleDarkModeClick() {
     setIsDarkMode((isDarkMode) => !isDarkMode);
   }
 
+  function handleFormSubmit(e) {
+    e.preventDefault();
+    const newItem = {
+      id: uuid(),
+      name,
+      category
+    }
+    setItems([...items, newItem]);
+  }
+
+  function onNameChange(e) {
+    setName(e.target.value);
+  }
+
+  function onNewCategoryChange(e) {
+    setCategory(e.target.value);
+  }
+
   return (
     <div className={"App " + (isDarkMode ? "dark" : "light")}>
       <Header isDarkMode={isDarkMode} onDarkModeClick={handleDarkModeClick} />
-      <ShoppingList items={items} />
+      <ShoppingList items={items} onItemFormSubmit={handleFormSubmit} name={name} category={category} onNameChange={onNameChange} onNewCategoryChange={onNewCategoryChange} />
     </div>
   );
 }
--- src/data/items.js
+++ src/data/items.js
@@ -1,3 +1,4 @@
+
 const items = [
   { id: 1, name: "Yogurt", category: "Dairy" },
   { id: 2, name: "Pomegranate", category: "Produce" },
